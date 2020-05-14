31780d2705a4301ac52d26e0b951a849d59df251
Jan Strube <jan.strube@gmail.com>
2020-05-13 20:38:20 -0700

    Adding support for ValenciaPlugin

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,8 @@
+using CxxWrap: StdVector
 using FastJet
-using CxxWrap
+using Test
 
-# this is the example from http://www.fastjet.fr/quickstart.html
-function main()
-    particles = PseudoJet[]
-    # an event with 3 particles:  px    py   pz   E
-    push!(particles, PseudoJet(  99.0,  0.1, 0, 100.0))
-    push!(particles, PseudoJet(   4.0, -0.1, 0,   5.0))
-    push!(particles, PseudoJet( -99.0,    0, 0,  99.0))
-  
-    # choose a jet definition
-    R = 0.7
-    jet_def = JetDefinition(antikt_algorithm, R)
-    # run the clustering, extract the jets
-    cs = ClusterSequence(StdVector(particles), jet_def)
-    jets = inclusive_jets(cs, 0.0)
-
+function printInfo(jets)
     # print out some infos
     # println("Clustering with ", description(jet_def))
     # print the jets
@@ -27,6 +14,34 @@ function main()
         #     println("    constituent ", j, "'s pt: ", pt(constituents[j]))
         # end
     end
-    return cs
+end
+
+# this is the example from http://www.fastjet.fr/quickstart.html
+function main()
+    particles = PseudoJet[]
+    # an event with 3 particles:  px    py   pz   E
+    push!(particles, PseudoJet(  99.0,  0.1, 0, 100.0))
+    push!(particles, PseudoJet(   4.0, -0.1, 0,   5.0))
+    push!(particles, PseudoJet( -99.0,    0, 0,  99.0))
+  
+    # choose a jet definition
+    @testset "Core functionality" begin
+        R = 0.7
+        jet_def = JetDefinition(antikt_algorithm, R)
+        # run the clustering, extract the jets
+        cs = ClusterSequence(StdVector(particles), jet_def)
+        jets = inclusive_jets(cs, 0.0)
+        @test length(jets) == 2
+        printInfo(jets)
+    end
+    @testset "Plugins" begin
+        vp = FastJet.ValenciaPlugin(1.2, 0.8)
+        jet_def = JetDefinition(vp)
+        # run the clustering, extract the jets
+        cs = ClusterSequence(StdVector(particles), jet_def)
+        jets = inclusive_jets(cs, 0.0)
+        @test length(jets) == 2
+        printInfo(jets)
+    end
 end 
 main() 
